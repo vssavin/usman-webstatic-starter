cf359910e1b5796ea401cbacf890abb51540fc72
vssavin <savin1801@gmail.com>
2023-12-28 17:08:38 +0300

    spring5/6 tests: added test to check context loading

---
+++ usman-webstatic-starter-spring6/src/test/java/com/github/vssavin/usman_webstatic_starter/spring6/StartingSpringBootTest.java
@@ -0,0 +1,22 @@
+package com.github.vssavin.usman_webstatic_starter.spring6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * @author vssavin on 28.12.2023.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringBootTest
+public class StartingSpringBootTest {
+
+    @Test
+    public void contextLoads() {
+
+    }
+
+}
